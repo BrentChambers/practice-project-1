18038e1d411ab81b52782a621262e2dbecc9a4dc
Brent Chambers <brent.program@gmail.com>
2020-03-28 21:01:56 -0400

    Load speed fixed, zero padding on seconds, array for month and day

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # practice-project-1
 
 - Menu screen and toggle added.
+- Zero padding on seconds.
+- Time delay on load fixed.
+- Array added for month and day.
--- script/script.js
+++ script/script.js
@@ -1,18 +1,38 @@
 /*jshint esversion: 6 */
 (function(){
   "use strict";
+  let userPref = {
+    displaySeconds: false
+  };
 
-  //Menu buton
+  if(userPref.displaySeconds){
+    console.log('trueeee!');
+  }
+
+  function assemble(){
+
+  }
+
+  //Menu button
   let menuButton = document.querySelector('.menu-button');
   let menuScreen = document.querySelector('.menu');
   menuButton.addEventListener('click', function(){
     menuScreen.classList.toggle('displayFullScreen');
   });
 
+  let monthArr = [
+    'January','February','March','April',
+    'May','June','July','August',
+    'September','October','November','December'
+  ];
+
+  let dayArr = [
+    'Sunday', 'Monday', 'Tuesday','Wednesday',
+    'Thursday', 'Friday', 'Saturday'
+  ];
 
 
-
-  setInterval(function(){
+  function getTime(){
     let timeDisplay = document.querySelector('#time');
     let dateDisplay = document.querySelector('#date');
 
@@ -23,8 +43,8 @@
     let s = d.getSeconds();
     let amPm = h >= 12 ? 'PM' : 'AM';
 
-    let day = d.getDay() + 1;
-    let month = d.getMonth() + 1;
+    let day = d.getDay();
+    let month = d.getMonth();
     let monthDate = d.getDate();
     let year = d.getFullYear();
 
@@ -39,92 +59,16 @@
       m = String(m).padStart(2, 0);
     }
 
-    //Convert day number to name
-    switch(day){
-      case 1:
-      day = 'Sunday';
-      break;
-
-      case 2:
-      day = 'Monday';
-      break;
-
-      case 3:
-      day = 'Tuesday';
-      break;
-
-      case 4:
-      day = 'Wednesday';
-      break;
-
-      case 5:
-      day = 'Thursday';
-      break;
-
-      case 6:
-      day = 'Friday';
-      break;
-
-      case 7:
-      day = 'Saturday';
-      break;
-    }
-
-
-    //Convert month number to name.
-    switch(month){
-      case 1:
-      month = "January";
-      break;
-
-      case 2:
-      month = "February";
-      break;
-
-      case 3:
-      month = "March";
-      break;
-
-      case 4:
-      month = "April";
-      break;
-
-      case 5:
-      month = "May";
-      break;
-
-      case 6:
-      month = "June";
-      break;
-
-      case 7:
-      month = "July";
-      break;
-
-      case 8:
-      month = "August";
-      break;
-
-      case 9:
-      month = "September";
-      break;
-
-      case 10:
-      month = "October";
-      break;
-
-      case 11:
-      month = "November";
-      break;
-
-      case 12:
-      month = "December";
-      break;
+    //Padding second number
+    if(s < 10){
+      s = String(s).padStart(2, 0);
     }
 
     //Assemble time and date and display to the DOM
     timeDisplay.textContent = `${h}:${m}:${s} ${amPm}`;
-    dateDisplay.textContent = `${day}, ${month} ${monthDate}, ${year}`;
+    dateDisplay.textContent = `${dayArr[day]}, ${monthArr[month]} ${monthDate}, ${year}`;
+  }
 
-  }, 1000);
+  getTime();
+  setInterval(getTime, 1000);
 })();
